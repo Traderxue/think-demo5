4a9e41a7ee3354e5f6c8ac38d3cba2fd2c209182
Traderxue <falethe@gmail.com>
2023-11-26 23:07:27 +0800

    init

--- app/controller/User.php
+++ app/controller/User.php
@@ -4,7 +4,12 @@ namespace app\controller;
 use app\BaseController;
 use think\Request;
 use app\model\User as UserModel;
+use app\util\Res;
 
 class User extends BaseController{
-    
+    private $result;
+
+    public function __construct(){
+        $this->result = new Res();
+    }
 }
